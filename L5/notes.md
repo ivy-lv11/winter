aad4cf5991761df49d2652d1ffa4fdf99fb6dbf1
ivy-lv11 <181220038@smail.nju.edu.cn>
2020-03-12 11:58:36 +0800

    Lecture

---
+++ L5/notes.md
@@ -0,0 +1,6 @@
+python3 进入Python  
+ctrl-D 退出python  
+sh matrace.sh ./a.out 执行脚本文件  
+ltrace追踪库函数调用  
+strace追踪系统调用  
+
--- L5/notes_test/build/x86_64-qemu/src/thread-os-mp.d
+++ L5/notes_test/build/x86_64-qemu/src/thread-os-mp.d
@@ -1,5 +1,5 @@
-/home/lv/os-workbench/notes_test/build/x86_64-qemu//./src/thread-os-mp.o: \
- /home/lv/os-workbench/notes_test/src/thread-os-mp.c \
- /home/lv/os-workbench/notes_test/../abstract-machine/am/am.h \
- /home/lv/os-workbench/notes_test/../abstract-machine/am/include/arch/x86_64-qemu.h \
- /home/lv/os-workbench/notes_test/../abstract-machine/libs/klib/include/klib.h
+/home/lv/winter/winter/L5/notes_test/build/x86_64-qemu//./src/thread-os-mp.o: \
+ /home/lv/winter/winter/L5/notes_test/src/thread-os-mp.c \
+ /home/lv/winter/winter/L5/notes_test/../abstract-machine/am/am.h \
+ /home/lv/winter/winter/L5/notes_test/../abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/lv/winter/winter/L5/notes_test/../abstract-machine/libs/klib/include/klib.h
